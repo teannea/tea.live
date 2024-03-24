0368a5ba5b27b2196aa9ed56be7774dbd8068809
teannea <ci6nk8cr@duck.com>
2024-03-25 04:50:51 +0800

    add more bloat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import UnoCSS from "unocss/astro"
+
+import react from "@astrojs/react"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
+})
